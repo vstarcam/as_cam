93de8e37105449b527d8abb9a1ab75722c042687
Joe <372405333@qq.com>
2018-03-15 14:03:18 +0800

    support vstf

--- IPCamerDemo/src/com/ipcamera/demo/bean/PlayBackBean.java
+++ IPCamerDemo/src/com/ipcamera/demo/bean/PlayBackBean.java
@@ -2,30 +2,45 @@ package com.ipcamera.demo.bean;
 
 import java.io.Serializable;
 
-public class PlayBackBean implements Serializable{
- /**
+public class PlayBackBean implements Serializable {
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private String did;
 	private String path;
 	private int videotime;
+	private int filesize;
+
 	public String getDid() {
 		return did;
 	}
+
 	public void setDid(String did) {
 		this.did = did;
 	}
+
 	public String getPath() {
 		return path;
 	}
+
 	public void setPath(String path) {
 		this.path = path;
 	}
-	public void setVideotime(int time){
-		this.videotime=time;
+
+	public void setVideotime(int time) {
+		this.videotime = time;
 	}
-	public int getVideotime(){
+
+	public int getVideotime() {
 		return videotime;
 	}
+	
+	public void setVideofilesize(int filesize) {
+		this.filesize = filesize;
 	}
+
+	public int getVideofilesize() {
+		return filesize;
+	}
+}
